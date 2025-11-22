62f8aabfb0241f29b3374b0cd753b0f48a9c3dbc
Shubham Kumar (Product Architecture) <unresolved.shubham@gmail.com>
2025-11-22 17:16:24 +0530

    PACT for provider kitchen Server App

---
+++ src/test/java/code/shubham/kitchenserver/PactProviderTest.java
@@ -0,0 +1,19 @@
+package code.shubham.kitchenserver;
+
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@Provider("KitchenServer")
+@PactBroker
+public class PactProviderTest {
+
+  @TestTemplate
+  @ExtendWith(PactVerificationInvocationContextProvider.class)
+  void verifyPacts(PactVerificationContext context) {
+    context.verifyInteraction();
+  }
+}
